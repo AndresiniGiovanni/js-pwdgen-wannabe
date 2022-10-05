d2f6292ecaa4db7b2e152c9c8fd73be7fa57e146
AndresiniGiovanni <gio.andresini@hotmail.it>
2022-10-05 15:12:12 +0200

    password generator

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@
 // poi chiedi la sua età
 // Infine scrivi sulla pagina nomecognomecolorepreferito21 (dove il 21 è in questo caso l'età)
 
+const nomepersona = prompt("Come ti chiami ?");
+const cognome = prompt("Il tuo cognome ?");
+const colorepreferito = prompt("Il tuo colore preferito ?");
+const eta = prompt("La tua etá ?");
+const persona = nomepersona + cognome + colorepreferito + eta;
+const elemento = document.getElementById("persona");
+elemento.innerHTML = persona;
